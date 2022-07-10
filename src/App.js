56ee86fff8d76a3021dfb319afd7ac73c47156b6
Leno rafael <73203800+lenors@users.noreply.github.com>
2022-07-10 10:37:23 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+ import React, {Component} from 'react';
+ class App extends Component {
+  state = {
+     nome: ['leno'],
+     idade:[22],
+     comida:['bife'],
+     
+   }
+   render(){
+    return(
+       <div>
+       <h1>{this.state.nome}</h1>
+       <h2>{this.state.idade}</h2>
+       <h3>{this.state.comida}</h3>
+       </div>
+     )
+   }
+ }
 
 export default App;
